4c6e98d2049285a45dcdda00649b3e713d90598d
Javier Uruen Val <juruen@warp.es>
2011-03-05 13:38:47 +0100

    Set version to 1.0.0
    Getting ready for tag and release.

--- desktop_droid/__init__.py
+++ desktop_droid/__init__.py
@@ -1,6 +1,6 @@
 __appname__     = 'desktop-droid'
 __progname__    = 'Desktop-Droid'
-__version__     = '1.0'
+__version__     = '1.0.0'
 __description__ = 'A small desktop app to receive telephony events from android phones'
 __copyright__   = 'Copyright (c) 2011 Warp Networks, S.L'
 __homepage__    = 'https://github.com/foolano/desktop-droid-mobile'
